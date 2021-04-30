9450302c6bdd0e1e53f682ed34eeb084e4943792
Rambabu Patina <Rambabu.Patina@appfire.com>
2021-04-30 09:15:42 +0530

    Clean up

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ const fs = require('fs');
 const utils = require('./utils');
 
 const jsonStream = StreamArray.withParser();
-//var count = 0;
+
 var table = [];
-//You'll get json objects here
-//Key is an array-index here
+
 jsonStream.on('data', ({key, value}) => {
     let col = utils.addColumns(value);
     if (col) {
         table.push(col);
     }
 });
-
-
 
 jsonStream.on('end', () => {
     console.log('Count of Overweight people is: ' + table.filter(x => x.BMICategeory == "Overweight").length);
